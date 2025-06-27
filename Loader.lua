d099c612697e0dbce571f0a3bec5a8b96a578fbf
chromeheartslens <100755166+chromeheartslens@users.noreply.github.com>
2025-06-27 19:26:28 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 -- // main loader, script later
--- // utilized to load EVERY other function
+-- // utilized to load EVERY other function ( make sure to pcall every call so it doesnt show on developer's console )
 
 
 return true
